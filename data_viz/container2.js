fb28190c280e1be0ec9b7764054e58ae0e8e437c
Evan Velasco <evan.velasco@instacart.com>
2025-03-30 01:17:22 -0400

    resolved duplicate const and cleaned up shell

--- data_viz/container2.js
+++ data_viz/container2.js
@@ -1,4 +1,4 @@
-const data = [
+const data2 = [
     { date: new Date(2025, 0, 1), value: 30 },
     { date: new Date(2025, 1, 1), value: 50 },
     { date: new Date(2025, 2, 1), value: 40 },
@@ -6,53 +6,45 @@ const data = [
     { date: new Date(2025, 4, 1), value: 80 }
 ];
 
+const width2 = 300;
+const height2 = 200;
+const marginTop2 = 30;
+const marginRight2 = 40;
+const marginBottom2 = 30;
+const marginLeft2 = 40;
 
-const width = 200;
-const height = 200;
-const marginTop = 30;
-const marginRight = 0;
-const marginBottom = 30;
-const marginLeft = 40;
-
-const svg = d3.select("#container2")
+const svg2 = d3.select("#container2")
     .append("svg")
-    .attr("viewBox", `0 0 ${width} ${height}`) 
-    .attr("preserveAspectRatio", "xMidYMid meet") 
+    .attr("viewBox", `0 0 ${width2} ${height2}`)
+    .attr("preserveAspectRatio", "xMidYMid meet")
     .attr("width", "100%")
     .attr("height", "100%");
 
-const xScale = d3.scaleTime()
-    .domain(d3.extent(data, d => d.date))
-    .range([marginLeft, width - marginRight]);
+const xScale2 = d3.scaleTime()
+    .domain(d3.extent(data2, d => d.date))
+    .range([marginLeft2, width2 - marginRight2]);
 
-const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.value)])
-    .range([height - marginBottom, marginTop]);
+const yScale2 = d3.scaleLinear()
+    .domain([0, d3.max(data2, d => d.value)])
+    .range([height2 - marginBottom2, marginTop2]);
 
-const line = d3.line()
-    .x(d => xScale(d.date))
-    .y(d => yScale(d.value));
+const line2 = d3.line()
+    .x(d => xScale2(d.date))
+    .y(d => yScale2(d.value));
 
-svg.append("path")
-    .datum(data)
+svg2.append("path")
+    .datum(data2)
     .attr("fill", "none")
     .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("d", line);
+    .attr("stroke-width", 3)
+    .attr("d", line2);
 
+svg2.append("g")
+    .attr("transform", `translate(0,${height2 - marginBottom2})`)
+    .call(d3.axisBottom(xScale2)
+        .tickValues(data2.map(d => d.date))
+        .tickFormat(d3.timeFormat("%b %Y")));
 
-
-svg.append("g")
-    .attr("transform", `translate(0,${height - marginBottom})`)
-    .call(d3.axisBottom(x).tickSizeOuter(0));
-
-svg.append("g")
-    .attr("transform", `translate(${marginLeft},0)`)
-    .call(d3.axisLeft(y).tickFormat((y) => (y * 100).toFixed()))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.append("text")
-        .attr("x", -marginLeft)
-        .attr("y", 10)
-        .attr("fill", "currentColor")
-        .attr("text-anchor", "start")
-        .text("Frequency (%)"));
+svg2.append("g")
+    .attr("transform", `translate(${marginLeft2},0)`)
+    .call(d3.axisLeft(yScale2).tickFormat((y) => (y * 100).toFixed()))
